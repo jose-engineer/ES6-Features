4f4231f1a869946e79b5186a112923ec7c6e5ef1
jose-engineer <jose-engineer@outlook.com>
2022-10-02 20:37:28 -0500

    Maps

--- VariablesAndDataStructures/WritingMaps/finished/script.js
+++ VariablesAndDataStructures/WritingMaps/finished/script.js
@@ -4,15 +4,16 @@ course.set("react", { description: "ui" });
 course.set("jest", { description: "testing" });
 
 // console.log(course);
-// console.log(course.react);
+// console.log(course.react); //undefined
 // console.log(course.get("react"));
 
 let details = new Map([
-  [new Date(), "today"],
+  [new Date(), "today"], //keys can be any type, in this case a dynamic Date type
   [2, { javascript: ["js", "node", "react"] }],
   ["items", [1, 2]]
 ]);
 
+// console.log(details);
 // console.log(details.size);
 
 details.forEach(function (item) {
